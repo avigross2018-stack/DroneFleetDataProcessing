88759e49f47f12c1ef8e818e73431f9c88feeb15
avi-gross <avigross2018@gmail.com>
2026-07-14 11:10:00 +0300

    start class

---
+++ src/Models/Drone.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DroneFleetDataProcessing.src.Models
+{
+    public class Drone
+    {
+        public int Id { get; set;}
+        public string SerialNumber { get; set;}
+        public string Model { get; set;}
+        public string Category { get; set;}
+        
+    }
+}
